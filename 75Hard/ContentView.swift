285e904c58e952e1f4099f1a85003cadb098bfff
Julian Müller <julian1mueller@gmx.de>
2024-03-25 19:46:38 +0100

    Initial Commit

---
+++ 75Hard/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  75Hard
+//
+//  Created by Julian Müller on 25.03.24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ 75Hard/_5HardApp.swift
@@ -0,0 +1,17 @@
+//
+//  _5HardApp.swift
+//  75Hard
+//
+//  Created by Julian Müller on 25.03.24.
+//
+
+import SwiftUI
+
+@main
+struct _5HardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
